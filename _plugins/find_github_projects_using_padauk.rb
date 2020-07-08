1174887677209ab0f569ad0d82516e77fcef1a37
Christian Flach <cmfcmf.flach@gmail.com>
2020-07-08 10:28:40 +0200

    Improve layout

--- _plugins/find_github_projects_using_padauk.rb
+++ _plugins/find_github_projects_using_padauk.rb
@@ -11,25 +11,29 @@ module GitHubPadaukTopics
       @@cache ||= Jekyll::Cache.new("GitHubPadaukTopics")
     end
 
+    # Stringify all keys of a hash recursively
+    # This is needed since hashes passed to Jekyll are not processed correctly if they have symbol keys
+    def stringify_keys(h)
+      if h.is_a?(Hash) then
+        h = h.collect{|k,v| [k.to_s, stringify_keys(v)]}.to_h
+      elsif h.is_a?(Array) then
+        h = h.collect{|v| stringify_keys(v)}
+      end
+      h
+    end
+
     def generate(site)
       repos = cache.getset("repos") do
         client = Octokit::Client.new()
         Jekyll.logger.info "Fetching GitHub repositories with topic 'padauk'"
 
-        # https://docs.github.com/en/rest/reference/search#search-repositories
+        # https://docs.github.com/en/rest/referenceoc/search#search-repositories
         result = client.search_repositories(QUERY, {per_page: 100})
         repos = result.items.map(&:to_hash)
-        repos = repos.map { |repo| repo.select {
-          |key, value| ["full_name", "html_url", "description", "updated_at", "stargazers_count"].include?(key.to_s) }
-        }
-        # Convert symbol keys to string keys
-        # https://stackoverflow.com/a/36936333/2560557
-        repos = repos.map { |repo| repo.collect{|k,v| [k.to_s, v]}.to_h }
-
         repos
       end
 
-      site.config['projects_using_padauk'] = repos
+      site.config['projects_using_padauk'] = stringify_keys(repos)
       site.config['projects_using_padauk_query_url'] = "https://github.com/search?q=" + ERB::Util.url_encode(QUERY)
     end
   end
